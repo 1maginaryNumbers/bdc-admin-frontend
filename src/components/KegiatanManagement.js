5f9a959f623ff7305137c0ab2c552f21148f128d
Imagine- <simonsugiarta@gmail.com>
2025-11-27 02:01:30 +0700

    Add Kategori column to kegiatan table
    Introduces a new 'Kategori' column in the kegiatan management table to display the category name for each item. This improves data visibility and allows users to easily identify the category of each kegiatan.

--- src/components/KegiatanManagement.js
+++ src/components/KegiatanManagement.js
@@ -504,6 +504,7 @@ const KegiatanManagement = () => {
                 </th>
                 <th>Nama Kegiatan</th>
                 <th>Deskripsi</th>
+                <th>Kategori</th>
                 <th>Tanggal Mulai</th>
                 <th>Tanggal Selesai</th>
                 <th>Waktu</th>
@@ -527,6 +528,7 @@ const KegiatanManagement = () => {
                   <td style={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis' }}>
                     {item.deskripsi}
                   </td>
+                  <td>{item.kategori?.namaKategori || '-'}</td>
                   <td>{formatDate(item.tanggalMulai)}</td>
                   <td>{formatDate(item.tanggalSelesai)}</td>
                   <td>
